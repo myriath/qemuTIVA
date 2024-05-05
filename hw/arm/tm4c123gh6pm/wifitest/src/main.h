2f711e16409edea4a0c4e73f3212d7baa3476650
myriath <mitchhudson2002@gmail.com>
2024-05-04 20:40:27 -0500

    Moving to laptop, debugging test berkeley socket code

---
+++ hw/arm/tm4c123gh6pm/wifitest/src/main.h
@@ -0,0 +1,19 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define SUCCESS         0
+#define ERR_SOCK_FAILED -1
+#define ERR_CONNECT     -2
+#define ERR_BIND        -3
+#define ERR_LISTEN      -4
+#define ERR_ACCEPT      -5
+
+#endif
